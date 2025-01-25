bde9b9b08fae147ae4550f327db20b3eb0d91ee4
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-01-25 14:18:48 +0300

    🗳️ setup configs properly

---
+++ .config/build.config.ts
@@ -0,0 +1,15 @@
+import { BuildConfig, defineBuildConfig } from 'unbuild';
+
+const BASE_CONFIG: BuildConfig = {
+  outDir: './dist',
+  failOnWarn: false,
+  declaration: true,
+};
+
+export default defineBuildConfig([
+  {
+    ...BASE_CONFIG,
+    name: 'Build types submodule',
+    entries: ['./src/types/index.ts'],
+  },
+]);
--- .config/ts/build.base.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineBuildConfig } from 'unbuild';
-
-export default defineBuildConfig({
-  name: 'Base config for all build processes',
-  failOnWarn: false,
-  declaration: true,
-});
