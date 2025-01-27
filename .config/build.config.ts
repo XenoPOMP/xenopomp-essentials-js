5d296ac0b528d930c8c8c642c441ceafef764bd5
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-01-27 12:39:54 +0300

    🏗️ change config

--- .config/build.config.ts
+++ .config/build.config.ts
@@ -2,7 +2,14 @@ import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig([
   {
-    entries: ['./src/index.ts'],
+    entries: [
+      './src/index.ts',
+      {
+        builder: 'mkdist',
+        input: './src/declarations',
+        outDir: './dist/declarations',
+      },
+    ],
     outDir: './dist',
     failOnWarn: false,
     declaration: true,
