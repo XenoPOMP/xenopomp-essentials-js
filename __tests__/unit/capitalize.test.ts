cbfc7c6ca9f3aa38b92a821367299a75836403d1
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-01-27 15:56:30 +0300

    🎓 create capitalize util

---
+++ __tests__/unit/capitalize.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from 'vitest';
+
+import { capitalize, uncapitalize } from '../../src';
+
+describe('Capitalize/uncapitalize utilities', () => {
+  test('Capitalize works', () => {
+    const og = 'alex';
+    const cap = capitalize(og);
+    expect(cap).toBe('Alex');
+  });
+
+  test('Uncapitalize works', () => {
+    const og = 'Alex';
+    const cap = uncapitalize(og);
+    expect(cap).toBe('alex');
+  });
+});
---
+++ src/utils/capitalize.ts
@@ -0,0 +1,9 @@
+/** Capitalizes string (makes first letter uppercase). */
+export function capitalize<T extends string>(str: T): Capitalize<T> {
+  return (str.charAt(0).toUpperCase() + str.slice(1)) as Capitalize<T>;
+}
+
+/** Un-capitalizes string (makes first letter lowercase). */
+export function uncapitalize<T extends string>(str: T): Uncapitalize<T> {
+  return (str.charAt(0).toLowerCase() + str.slice(1)) as Uncapitalize<T>;
+}
