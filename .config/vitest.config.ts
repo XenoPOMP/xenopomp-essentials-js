a5d6129403a55c1d666f950f82fa164d4eee695a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-01-28 15:02:06 +0300

    📦 install vitest plugin

--- .config/vitest.config.ts
+++ .config/vitest.config.ts
@@ -1,8 +1,10 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 import { istanbulExclusions, vitestExclusions } from './testing-exclusions';
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     environment: 'jsdom',
     exclude: vitestExclusions,
