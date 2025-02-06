0069f6b7ac10b0f464bcdcb720b2e50f6bf0108e
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-02-06 12:18:18 +0200

    🌀 create type

---
+++ src/types/react/AnyFc.ts
@@ -0,0 +1,6 @@
+import type { FC } from 'react';
+
+/**
+ * Matches any proper React component.
+ */
+export type AnyFC = FC<unknown>;
--- src/types/react/index.ts
+++ src/types/react/index.ts
@@ -3,3 +3,5 @@ export * from './AsyncFC';
 export * from './SetState';
 export * from './FunctionalChildren';
 export * from './FcProps';
+export * from './AnyFc';
+
