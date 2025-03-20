4a3966f78fee1a59e1e8213568ca0c899355f259
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-20 18:34:55 +0200

    🧪 enhance tests

--- __tests__/unit/eslint/config.test.tsx
+++ __tests__/unit/eslint/config.test.tsx
@@ -1,9 +1,12 @@
 import { expectType } from 'tsd';
-import { describe, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 
 import type { Configs, Options, UserConfigItem } from '@/eslint';
 import xenopomp from '@/eslint';
+import { next } from '@/eslint/configs';
 import type { Fn } from '@/types';
+
+import { assertNotThrowing } from '@test/assets';
 
 type ExpectedType = Fn<
   [options?: Options, ...userConfigs: UserConfigItem[]],
@@ -13,5 +16,26 @@ type ExpectedType = Fn<
 describe('xenopomp ESLint config', () => {
   test('Expected types are not any', () => {
     expectType<ExpectedType>(xenopomp);
+  });
+
+  test('It does not throw', () => {
+    assertNotThrowing(() => xenopomp());
+
+    assertNotThrowing(() =>
+      xenopomp({
+        react: undefined,
+      }),
+    );
+
+    assertNotThrowing(() =>
+      xenopomp({
+        next: true,
+      }),
+    );
+  });
+
+  test('Next.js integration', async () => {
+    const config = await next();
+    expect(config.length).toBeGreaterThan(0);
   });
 });
