373fba3344583319a9ef6af7e1da12dcee8b71ad
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-05-28 17:37:48 +0200

    ✍️ define type

--- src/types/react/DataAttributes.ts
+++ src/types/react/DataAttributes.ts
@@ -1,4 +1,21 @@
 /**
- * @TODO
+ * Type that represents data-{attr} keys.
  */
-export type DataAttributes = unknown;
+type Attribute = `data-${string}`;
+
+/**
+ * Matches any proper data-{attr} keys.
+ */
+type DataAttributeShape = Record<Attribute, any>;
+
+/**
+ * Represents React`s data-{attr} properties for components.
+ *
+ * @example
+ * type AdditionalAttributes = DataAttributes<{ 'data-disabled': boolean }>;
+ *
+ * const attrs: AdditionalAttributes = {
+ *   'data-disabled': false, // this attr is required
+ * };
+ */
+export type DataAttributes<Shape extends DataAttributeShape> = Shape;
