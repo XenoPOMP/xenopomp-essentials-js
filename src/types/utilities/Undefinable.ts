4dbef0eb38161a5f7bb4b7aa3d5c6d24286ca227
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-11-27 17:42:14 +0300

    ✍️ rename type

--- src/types/utilities/Undefinable.ts
+++ src/types/utilities/Undefinable.ts
@@ -1,5 +1,11 @@
 /**
  * Makes type union of type T and undefined
  * @since 0.0.1
+ * @deprecated Renamed to {@link Optional}.
  */
 export type Undefinable<T> = T | undefined;
+
+/**
+ * Marks type as optional (undefinable).
+ */
+export type Optional<T> = T | undefined;
