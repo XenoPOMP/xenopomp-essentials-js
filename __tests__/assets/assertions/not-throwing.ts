a737484fe3830591616bc338d953e711f1404600
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-11-28 17:42:36 +0300

    📦 implement type by myself

--- __tests__/assets/assertions/not-throwing.ts
+++ __tests__/assets/assertions/not-throwing.ts
@@ -1,9 +1,8 @@
-import type { PromiseOr } from 'sass';
 import { expect } from 'vitest';
 
+import type { PromiseOr } from '@/types';
+
 /** Executes function and expects that it won't throw. */
-export const assertNotThrowing = <T = void>(
-  callable: () => PromiseOr<T, 'async'>,
-) => {
+export const assertNotThrowing = <T = void>(callable: () => PromiseOr<T>) => {
   expect(() => callable()).not.toThrow();
 };
--- src/types/aliases/index.ts
+++ src/types/aliases/index.ts
@@ -1,3 +1,4 @@
 export * from './AnyObject';
 export * from './EmptyObject';
 export * from './Fn';
+export * from './PromiseOr';
