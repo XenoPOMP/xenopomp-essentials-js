01db70a96dae20a84940b48bb222d67351e2b749
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-11-30 16:06:13 +0300

    📝 change config

--- .config/vitest.config.integrity.ts
+++ .config/vitest.config.integrity.ts
@@ -2,6 +2,10 @@ import { defineConfig, mergeConfig } from 'vitest/config';
 
 import rootConfig from './vitest.config';
 
-const integrityConfig = defineConfig({});
+const integrityConfig = defineConfig({
+  test: {
+    include: ['__tests__/integrity/**/*.(ts|tsx)'],
+  },
+});
 
 export default mergeConfig(rootConfig, integrityConfig);
