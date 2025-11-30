bd313b621a2fe20f437d3e734a886f8f4228be72
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-11-30 14:52:39 +0300

    ✍️ parse dotenv in schema

---
+++ src/schemas/env.schema.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import { z } from 'zod';
+
+// Parse .env into schema
+dotenv.config({ quiet: true });
+
+// Create type-safe schema
+const envSchema = z.object({
+  DISABLE_MINIFY: z.coerce.boolean(),
+});
+
+// Type-safe parsed `process.env` object.
+export const env = envSchema.parse(process.env);
