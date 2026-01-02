f014f5c3c509338573288fa5a5ddee76c9ab8e9f
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-02 10:19:51 +0200

    🐛 fix tests

--- __tests__/integrity/match-media.test.ts
+++ __tests__/integrity/match-media.test.ts
@@ -1,10 +1,6 @@
 import { describe, test } from 'vitest';
 
-import { injectMatchMediaMock } from '@/../dist/vitest';
-
 describe('MatchMediaMock is transpiled properly', () => {
-  injectMatchMediaMock();
-
   test('It works!', () => {
     // eslint-disable-next-line no-console
     console.debug('It works!');
